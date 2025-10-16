5c74a87910f0ed3a7af64868527c34846381dd52
Prashanth <prashant301k@gmail.com>
2025-10-16 17:24:43 +0530

    removed unwanted lin

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -10,8 +10,7 @@ class GeometryCalculator:
 
 if __name__ == "__main__":
     calculator = GeometryCalculator()
-
-# TODO: Implement the feature to calculate the area of a rectangle 
+ 
 length = 10
 
 width = 6
