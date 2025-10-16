7a5a7a2d4fe4a62c60929dfb3d9469a8bea621dd
Prashanth <prashant301k@gmail.com>
2025-10-16 16:06:19 +0530

    added circle area code

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -11,8 +11,8 @@ class GeometryCalculator:
 if __name__ == "__main__":
     calculator = GeometryCalculator()
 
-# TODO: Implement the feature to calculate the area of a rectangle # length = 10
+# TODO: Implement the feature to calculate the area of a circle
 
-width = 6
+radius = 5
 
-print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
